feccd9a804b41358b384328d216a5253ffd6f03a
blaws006 <blaws006>
2017-11-15 20:06:20 -0500

    More setup. Handlebar files initiated, and controller. js file created. Public folder created.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var burger = require("../models/buger");
+var router = express.Router();
+
+router.get("/", function(req, res){
+    burger.selectAll(function(data){
+        var allObject = {
+            burger_name: data
+        };
+        console.log(allObject);
+        res.render("index", allObject)
+    });
+});
+
+router.post("/api/burgers", function(req, res){
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [req.body.burger_name, req.body.devoured], function(result){
+        res.json({ id: res.insertId });
+    });
+});
+
+router.put("/api/bugers/:id", function(req, res){
+    var condition = "id - " + req.params.id;
+    console.log("condition", condition);
+    burger.udateOne(
+        {devoured: req.body.devoured}
+   , condition, function(result){
+    if(result.changedRows === 0){
+        return res.status(404).end();
+    } else {
+        return res.status(201).end();
+    }
+   });
+});
+
+module.exports = router;
