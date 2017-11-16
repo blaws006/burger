dce60287a7d8db3f1df720566063767e4735bae6
blaws006 <blaws006>
2017-11-15 22:29:22 -0500

    UPDATES: Can boot up handlebars. searchAll() works A couple of issues reamin: Add and update need to work, How do I get my effin images?!?!?!

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ app.use(bodyParser.json({
 // Static directory to be served
 app.use(express.static("app/public"));
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
 app.listen(port, function(){
     console.log("Listening at Port: " + port);
 });
