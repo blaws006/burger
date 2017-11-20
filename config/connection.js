a2e6b4bda4cba10ea856e24673eda376056f0d15
blaws006 <blaws006>
2017-11-19 20:30:47 -0500

    adding heroku variables.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
+//This connects us to the mysql database
 var mysql = require('mysql');
 
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'R3dsk1ns1!',
     database: 'burgers_db'
 });
-
+};
+//Connection and confirmation
 connection.connect(function(err){
     if (err){
         console.log("Error connecting: " + err.stack);
